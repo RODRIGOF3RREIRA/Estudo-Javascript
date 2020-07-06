282120bd0ff3e661746992efb476b573470fb689
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-07-06 18:42:01 -0400

    for com array

---
+++ EstruturasControle/for.js
@@ -0,0 +1,9 @@
+for ( let i = 1; i <= 10; i++){
+    console.log(`i = ${i}`)
+}
+
+
+const notas = [2.5, 7.6, 6.6, 8.6]
+for ( let i = 0; i < notas.length; i++){
+    console.log(`Notas: ${notas[i]}`)
+}
