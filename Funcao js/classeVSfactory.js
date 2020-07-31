53f57bbfe5c128450c57404f1408492714d9d701
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-07-30 21:44:01 -0300

    Update classeVSfactory.js

--- Funcao js/classeVSfactory.js
+++ Funcao js/classeVSfactory.js
@@ -2,15 +2,16 @@ class Pessoa {
     constructor(nome) {
         this.nome = nome
     }
-
     falar() {
         console.log(`Meu nome é ${this.nome}`)
     }
-
 }
     const p1 = new Pessoa ('João')
     p1.falar()
 
+
+
+    
     const pessoa = nome => {
         return {
             falar2: () => console.log(`Meu nome é ${nome}`)
