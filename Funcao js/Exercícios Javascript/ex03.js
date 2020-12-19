84759e69540a05f7858121d8aeaf389f79e6bce0
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-12-19 19:41:17 -0400

    Create ex03.js

---
+++ Funcao js/Exercícios Javascript/ex03.js
@@ -0,0 +1,11 @@
+/*
+Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+*/
+
+function conta(base, expoente) {
+
+    resultado = base ** expoente;
+    return resultado;
+};
+console.log(conta(3, 3))
+
