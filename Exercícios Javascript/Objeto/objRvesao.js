857c527c4bb52aab942c1c199164382a957c7e87
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2021-01-10 16:17:05 -0400

    Create objRvesao.js

---
+++ Exercícios Javascript/Objeto/objRvesao.js
@@ -0,0 +1,33 @@
+// coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+console.log(produto)
+
+delete produto.preco
+delete produto ['marca do produto']
+console.log(produto)
+
+cont carro = { 
+    modelo: 'A4',
+    valor: 89444,
+    proprietario: {
+        nome:'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero:123
+        }
+    },
+    condutores: [{
+        nome:'Junior',
+        idade:10
+    }, {
+        nome:'ana',
+        idade:22
+    }],
+    calcularValorSeguro: function() {
+        
+    }
+}
