0e70a09c120e052da52326abdb63f362b3336e66
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2021-01-11 21:42:19 -0400

    Create NotacaoLiteral.js

---
+++ Exercícios Javascript/Objeto/NotacaoLiteral.js
@@ -0,0 +1,18 @@
+const a = 1 
+const b = 2
+const c = 3
+
+const obj1 = {a: a, b: b, c: c} //possibila alterar o nome da constante 
+const obj2 = {a, b, c}
+console.log(obj1, obj2)
+
+const nomeAttr = 'nota'
+const valorAtttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAtttr
+console.log(obj3)
+
+const obj4 = {[nomeAttr]: valorAtttr}
+console.log(obj4)
+
