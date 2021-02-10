97cb7e9238613c77f8e4c1350fa3e2f4b91bea65
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2021-02-10 12:04:22 -0400

    Update ex17.js

--- Exercícios Javascript/Lista01/ex17.js
+++ Exercícios Javascript/Lista01/ex17.js
@@ -8,3 +8,23 @@ C       20%
 Faça uma função que leia o plano de trabalho e o salário atual de um funcionário e calcula e imprime o seu
 novo salário. Use a estrutura switch e faça um caso default que indique que o plano é inválido.
  */
+
+function almentoSalario(salario_Atual, plano) {
+    switch (plano) {
+        case 'A':
+            return salario_Atual * 1.1
+        case 'B':
+            return salario_Atual * 1.15
+        case 'C':
+            return salario_Atual * 1.2
+
+
+
+        default:
+            return "Erro"
+    }
+}
+console.log(almentoSalario(1000, 'A'))
+console.log(almentoSalario(1000, 'B'))
+console.log(almentoSalario(1000, 'C'))
+console.log(almentoSalario(1000, 'D'))
